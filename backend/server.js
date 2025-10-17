6cf5eacf939ccbf6465f397788c28c7b3d998b96
renaldis <renaldis142@gmail.com>
2025-10-17 18:17:27 +0700

    feat: add create rest api add transaction

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,15 @@ import { sql } from './config/db.js';
 dotenv.config();
 
 const app = express();
+// middleware, read json from req body
+app.use(express.json());
+
+// custom simple middleware
+// app.use((req, res, next) => {
+//   console.log('Hey we hit a req, the method is', req.method);
+//   next();
+// });
+
 const PORT = process.env.PORT || 5001;
 
 async function initDB() {
@@ -31,7 +40,30 @@ async function initDB() {
 }
 
 app.get('/', (req, res) => {
-  res.send('Its working!');
+  res.send('its working');
+});
+
+app.post('/api/transactions', async (req, res) => {
+  // title, amount, category, user_id
+  try {
+    const { title, amount, category, user_id } = req.body;
+
+    if (!title || !user_id || !category || amount === undefined) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    const transaction = await sql`
+    INSERT INTO transactions(user_id, title, amount, category)
+    VALUES (${user_id},${title},${amount},${category})
+    RETURNING *
+    `;
+
+    console.log(transaction);
+    res.status(201).json(transaction[0]);
+  } catch (error) {
+    console.log('Error creating transactions', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
 initDB().then(() => {
